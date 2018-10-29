dd69a3fb2b5a11cc18b4a3242b5518fa42e8461c
Max Snijders <max.snijders@gmail.com>
2018-10-29 11:46:46 +0100

    Initial Commit

---
+++ 2048/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2048
+//
+//  Created by Max Snijders on 29/10/2018.
+//  Copyright © 2018 Max Snijders. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
